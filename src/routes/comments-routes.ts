ed48244578ba7dff4ebfc83aa9c14dbfed116e12
awadhoothsk <awadhooths16@gmail.com>
2025-03-28 11:18:53 +0530

    refactor: update import paths to use .js extensions for consistency

--- src/routes/comments-routes.ts
+++ src/routes/comments-routes.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
-import { tokenMiddleware } from "./middlewares/token-middleware";
-import { createComment, deleteComment, getComments, updateComment } from "../controllers/comments/comments-controller";
-import { CommentError } from "../controllers/comments/comments-types";
+import { tokenMiddleware } from "./middelwares/token-middelware.js";
+import { createComment, deleteComment, getComments, updateComment } from "../controllers/comments/comments-controller.js";
+import { CommentError } from "../controllers/comments/comments-types.js";
 
 export const commentsRoutes = new Hono();
 
